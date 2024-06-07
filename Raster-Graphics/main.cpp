ec22dd76b54a7a3e4bd853b87bbd8a3d772b1e35
Slavi15 <slavirezashkiofficial@gmail.com>
2024-06-08 01:01:33 +0300

    Add main.cpp for testing purposes

--- Raster-Graphics/main.cpp
+++ Raster-Graphics/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
+//#include "Image/Image.h"
+#include "Image/PBMImage/PBM1Image.h"
+#include "Image/PBMImage/PBM4Image.h"
+
 int main()
 {
+	PBM4Image image("Files/sample_hd.pbm");
 
+	Image* ptr = &image;
 
-    return 0;
+	ptr->load();
+
+	//ptr->applyNegative();
+
+	ptr->rotateLeft();
+
+	ptr->save("Files/hdtest.pbm");
+
+	return 0;
 }
